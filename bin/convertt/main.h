30feeb7f09a48c0e727f8b0d0786a6b3c8d31d5b
Fathma Mehnoor <fathmamehnoor@gmail.com>
2023-03-29 20:10:04 +0530

    Add function for writing of tiff files
    Until this commit, writing tiff files was not supported. Thus, it was not
    possible to convert other image and data formats to tiff file using astconvertt.
    With this commit, writing tiff files and converting other image and data formatsto tiff file using astconverrt is provided.
    
    This fixes task #16096: https://savannah.gnu.org/task/?16096

--- bin/convertt/main.h
+++ bin/convertt/main.h
@@ -47,6 +47,7 @@ enum output_formats
   OUT_FORMAT_PDF,
   OUT_FORMAT_FITS,
   OUT_FORMAT_JPEG,
+  OUT_FORMAT_TIFF,
 };
 
 enum colorspace_names
@@ -118,6 +119,10 @@ struct converttparams
   uint8_t      sizeinarcsec;  /* Sizes are in arcseconds (in WCS-mode).*/
   uint8_t      sizeinarcmin;  /* Sizes are in arcminutes (in WCS-mode).*/
   uint8_t marktextprecision;  /* Precision to convert floats.          */
+  int             widthinpx;  /* Width of image in pixel.              */
+  int            heightinpx;  /* Height of image in pixel.             */
+  int         bitspersample;  /* Number of bits used to represent color 
+                                 of a single pixel.                    */
 
   /* Internal */
   struct change     *change;  /* The value conversion string.          */
