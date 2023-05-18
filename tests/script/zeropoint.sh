2f478270ba10ae6893beb3099a531573f499ff26
Sepideh Eskandarlou <sepideh.eskandarlou@gmail.com>
2023-05-18 19:38:59 +0200

    astscript-zeropoint: check test of zero point script is added
    Until now, 'astscript-zeropoint' did not have a test, when running 'make
    check'.
    
    With this commit, all the things for checking the 'astscript-zeropoint'
    script is prepared and the check test is done. To do the test it was
    necessary to create another test for 'asttable' to make a reference catalog
    and star catalog (which are same).
    
    Thanks to Mohammad Akhlaghi for helping me to add this test.

---
+++ tests/script/zeropoint.sh
@@ -0,0 +1,89 @@
+# Check the zeropoint script.
+#
+# See the Tests subsection of the manual for a complete explanation
+# (in the Installing gnuastro section).
+#
+# Original author:
+#     Sepideh Eskandarlou <sepideh.eskandarlou@gmail.com>
+# Contributing author(s):
+#     Mohammad Akhlaghi <mohammad@akhlaghi.org>
+# Copyright (C) 2021, Free Software Foundation, Inc.
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
+
+
+
+
+# Preliminaries
+# =============
+#
+# Set the variables (the executable is in the build tree). Do the
+# basic checks to see if the executable is made or if the defaults
+# file exists (basicchecks.sh is in the source tree).
+prog=zeropoint
+execname=../bin/script/astscript-$prog
+
+dep1name=$progbdir/astfits
+dep2name=$progbdir/asttable
+dep3name=$progbdir/astmatch
+dep4name=$progbdir/astmkprof
+dep5name=$progbdir/astmkcatalog
+
+intable=table-img-to-wcs.fits
+inimg=convolve_spatial_scaled_noised.fits
+
+
+
+
+
+
+# Skip?
+# =====
+#
+# If the dependencies of the test don't exist, then skip it. There are two
+# types of dependencies:
+#
+#   - The executable script was not made.
+#   - The programs it use weren't made.
+#   - The input data weren't made.
+if [ ! -f $execname ]; then echo "$execname doesn't exist."; exit 77; fi
+if [ ! -f $dep1name ]; then echo "$dep1name doesn't exist."; exit 77; fi
+if [ ! -f $dep2name ]; then echo "$dep2name doesn't exist."; exit 77; fi
+if [ ! -f $dep3name ]; then echo "$dep3name doesn't exist."; exit 77; fi
+if [ ! -f $dep4name ]; then echo "$dep4name doesn't exist."; exit 77; fi
+if [ ! -f $dep5name ]; then echo "$dep5name doesn't exist."; exit 77; fi
+if [ ! -f $intable  ]; then echo "$intable doesn't exist."; exit 77; fi
+if [ ! -f $inimg    ]; then echo "$inimg doesn't exist."; exit 77; fi
+
+
+
+
+
+
+# Actual test script
+# ==================
+#
+# 'check_with_program' can be something like Valgrind or an empty
+# string. Such programs will execute the command if present and help in
+# debugging when the developer doesn't have access to the user's system.
+#
+# Since we want the script to recognize the programs that it will use from
+# this same build of Gnuastro, we'll add the current directory to PATH.
+export PATH="$progbdir:$PATH"
+
+# Test the script: finding the zeropoint.
+$check_with_program $execname \
+                    $inimg \
+                    --hdu=1 \
+                    --keepzpap \
+                    --refcatra=RA \
+                    --refcatdec=DEC \
+                    --refcat=$intable \
+                    --starcat=$intable \
+                    --aperarcsec=0.09,0.13 \
+                    --refcatmag=Magnitude \
+                    --output=zeropoint-cat.fits
--- tests/table/arith-img-to-wcs.sh
+++ tests/table/arith-img-to-wcs.sh
@@ -1,9 +1,4 @@
-# Create a mock image from cat1.txt:
-#
-#  - It includes one large and bright profile that is on the last
-#    pixel of this image. The other tests will also build the same
-#    profile with the absolute place fixed.
-#
+# Check conversion of coordinates.
 #
 # See the Tests subsection of the manual for a complete explanation
 # (in the Installing gnuastro section).
@@ -11,6 +6,7 @@
 # Original author:
 #     Mohammad Akhlaghi <mohammad@akhlaghi.org>
 # Contributing author(s):
+#     Sepideh Eskandarlou <sepideh.eskandarlou@gmail.com>
 # Copyright (C) 2015-2023 Free Software Foundation, Inc.
 #
 # Copying and distribution of this file, with or without modification,
@@ -19,16 +15,19 @@
 # without any warranty.
 
 
+
+
+
 # Preliminaries
 # =============
 #
 # Set the variables (The executable is in the build tree). Do the
 # basic checks to see if the executable is made or if the defaults
 # file exists (basicchecks.sh is in the source tree).
-prog=mkprof
+prog=table
 execname=../bin/$prog/ast$prog
-cat=$topsrc/tests/$prog/mkprofcat1.txt
-
+inimg=convolve_spatial_scaled_noised.fits
+intable=$topsrc/tests/mkprof/mkprofcat1.txt
 
 
 
@@ -39,11 +38,13 @@ cat=$topsrc/tests/$prog/mkprofcat1.txt
 # If the dependencies of the test don't exist, then skip it. There are two
 # types of dependencies:
 #
-#   - The executable was not made (for example due to a configure option).
+#   - The executable was not made (for example due to a configure option),
 #
-#   - Catalog doesn't exist (problem in tarball release).
+#   - The input data was not made (for example the test that created the
+#     data file failed).
 if [ ! -f $execname ]; then echo "$execname not created."; exit 77; fi
-if [ ! -f $cat      ]; then echo "$cat does not exist.";   exit 77; fi
+if [ ! -f $intable  ]; then echo "$intable doesn't exist.";  exit 77; fi
+if [ ! -f $inimg    ]; then echo "$inimg doesn't exist.";  exit 77; fi
 
 
 
@@ -52,10 +53,17 @@ if [ ! -f $cat      ]; then echo "$cat does not exist.";   exit 77; fi
 # Actual test script
 # ==================
 #
-# 'check_with_program' can be something like Valgrind or an empty
+# 'check_with_program' can be something like 'Valgrind' or an empty
 # string. Such programs will execute the command if present and help in
 # debugging when the developer doesn't have access to the user's system.
-$check_with_program $execname $cat --mergedsize=100,100 \
-                    --output=mkprofcat1.fits --zeropoint=0 \
-    && mv 0_mkprofcat1.fits psf.fits \
-    && mv 1_mkprofcat1.fits psf-sharp.fits
+#
+# This table will be used for checking the zeropoint script. Be carful
+# about column metadata.
+$check_with_program $execname $intable \
+                    --wcsfile=$inimg \
+                    --equal=Function,point \
+                    --output=table-img-to-wcs.fits \
+                    -c'arith X Y img-to-wcs',Magnitude \
+                    --colmetadata=2,DEC,deg,"Declination" \
+                    --colmetadata=1,RA,deg,"Right Ascension" \
+                    --colmetadata=3,Magnitude,log,"Magnitude of profile"
